83da6fb6fafec4084853ca2ff97f7aa9f3fa37a4
yogo254 <yogo254@yahoo.com>
2021-06-03 12:21:07 +0300

    fix add product ,service

--- src/main/java/com/prismhealth/dto/Request/CategoryRequest.java
+++ src/main/java/com/prismhealth/dto/Request/CategoryRequest.java
@@ -1,7 +1,7 @@
 package com.prismhealth.dto.Request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +11,12 @@ import lombok.NoArgsConstructor;
 @Data
 @JsonIgnoreProperties
 public class CategoryRequest {
-    @JsonProperty("categoryType")
+
     private String categoryType;
-    @JsonProperty("CategoryName")
+
     private String categoryName;
-    @JsonProperty("description")
+
     private String description;
-    @JsonProperty("photo")
+
     private String photo;
 }
