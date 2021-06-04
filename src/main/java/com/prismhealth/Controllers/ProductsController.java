07f218435cc1c8edb6585875e0fc002d5e74bbec
yogo254 <yogo254@yahoo.com>
2021-06-04 15:31:42 +0300

    add admin metrics

--- src/main/java/com/prismhealth/Controllers/ProductsController.java
+++ src/main/java/com/prismhealth/Controllers/ProductsController.java
@@ -13,7 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
