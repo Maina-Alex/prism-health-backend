43eb504de7739a938a287f22fa8dcca5398cf1be
yogo254 <yogo254@yahoo.com>
2021-06-04 15:21:29 +0300

    add admin metrics

---
+++ src/main/java/com/prismhealth/Controllers/AdminMetricControler.java
@@ -0,0 +1,39 @@
+package com.prismhealth.Controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.prismhealth.repository.BookingsRepo;
+import com.prismhealth.repository.ProductsRepository;
+import com.prismhealth.repository.ServiceRepo;
+import com.prismhealth.services.AdminProviderService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/metric")
+@AllArgsConstructor
+public class AdminMetricControler {
+
+    private BookingsRepo bookings;
+    private ServiceRepo serviceRepo;
+    private ProductsRepository productsRepository;
+    private AdminProviderService provider;
+
+    @GetMapping
+    public Map<String, Long> getMetrics() {
+        Map<String, Long> map = new HashMap<>();
+
+        map.put("bookings", bookings.count());
+        map.put("services", serviceRepo.count());
+        map.put("products", productsRepository.count());
+        map.put("providers", Long.valueOf("" + provider.getAllProviders().size()));
+        return map;
+
+    }
+
+}
